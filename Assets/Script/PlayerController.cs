7f6afeb18ce80ebd66a479d87b0c9845a5db0c09
zgulen <gulenzubeyir@gmail.com>
2023-02-04 13:46:44 +0300

    player bounded, new materials added

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float xRange = 12;
+    private float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //! Keeps The player in bounds
+        if (transform.position.x < -xRange){
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        }else if (transform.position.x > xRange){
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
