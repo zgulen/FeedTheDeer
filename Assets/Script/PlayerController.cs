a8b4b14c4f2b6b0c0755e94ff322de76ccfc3b62
zgulen <gulenzubeyir@gmail.com>
2023-02-04 18:13:00 +0300

    fix : prefabs, animals

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float xRange = 12;
     private float speed = 10.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +24,13 @@ public class PlayerController : MonoBehaviour
         }else if (transform.position.x > xRange){
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-    }
+        
+        if (Input.GetKeyDown(KeyCode.Space)){
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        }
 }
